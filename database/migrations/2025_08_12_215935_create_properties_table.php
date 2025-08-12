94f09e93461e811e7edc8649389f861eabd3e233
Rida Kataya <rkataya35@gmail.com>
2025-08-13 01:51:42 +0300

    update migrations

---
+++ database/migrations/2025_08_12_215935_create_properties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('host_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->boolean('verified')->default(false);
+            $table->timestamps();
+            $table->foreign('host_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
